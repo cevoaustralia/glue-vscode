fd98f0d648332215fe1cfb66e3a2dae825aea6d4
Jaehyeon Kim <dottami@gmail.com>
2021-10-19 14:59:32 +1100

    update container config

--- execute.sh
+++ execute.sh
@@ -1,34 +1,23 @@
 #!/usr/bin/env bash
 
 ## configure python runtime
-if [ "$version" == "1" ]; then
-  pyspark_python=python
-elif [ "$version" == "2" ]; then
-  pyspark_python=/root/venv/bin/python
-else
-  echo "unsupported version - $version, only 1 or 2 is accepted"
-  exit 1
-fi
+pyspark_python=python
 echo "pyspark python - $pyspark_python"
 
+## remove first argument
 execution=$1
 echo "execution type - $execution"
 
-## remove first argument
 shift 1
 echo $@
 
 ## set up command
 if [ $execution == 'pyspark' ]; then
-  sudo su -c "PYSPARK_PYTHON=$pyspark_python /home/aws-glue-libs/bin/gluepyspark"
+  sudo su -c "PYSPARK_PYTHON=$pyspark_python /opt/aws-glue-libs-3.0/bin/gluepyspark"
 elif [ $execution == 'spark-submit' ]; then
-  sudo su -c "PYSPARK_PYTHON=$pyspark_python /home/aws-glue-libs/bin/gluesparksubmit $@"
+  sudo su -c "PYSPARK_PYTHON=$pyspark_python /opt/aws-glue-libs-3.0/bin/gluesparksubmit $@"
 elif [ $execution == 'pytest' ]; then
-  if [ $version == "1" ]; then
-    sudo su -c "PYSPARK_PYTHON=$pyspark_python /home/aws-glue-libs/bin/gluepytest $@"
-  else
-    sudo su -c "PYSPARK_PYTHON=$pyspark_python /home/aws-glue-libs/bin/gluepytest2 $@"
-  fi
+  sudo su -c "PYSPARK_PYTHON=$pyspark_python /opt/aws-glue-libs-3.0/bin/gluepytest $@"
 else
   echo "unsupported execution type - $execution"
   exit 1
